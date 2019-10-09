f3133a1ad856e4269abb32249a75221520718711
Aaron <plazasimo@gmail.com>
2019-10-09 14:04:34 +0200

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+http
+    .createServer((req, res) => {
+        res.writeHead(200, { "Content-Type": "application/json" });
+
+        let salida = {
+            nombre: "Aarón",
+            edad: 40,
+            url: req.url
+        };
+
+        res.write(JSON.stringify(salida));
+        //res.write("Hola mundo");
+        res.end();
+    })
+    .listen(8080);
+
+console.log("escuchando el puerto 8080");
